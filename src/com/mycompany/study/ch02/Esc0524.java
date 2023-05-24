e47536c5a8f2b53a260732663b33c2214aed84b1
panggin123 <panggin159@naver.com>
2023-05-24 17:29:50 +0900

    asdf

---
+++ src/com/mycompany/study/ch02/Esc0524.java
@@ -0,0 +1,23 @@
+package com.mycompany.study.ch02;
+
+public class Esc0524 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HelloWord");
+		
+		int a = 1;
+		int b;
+		b = 2;
+		
+		
+		int c = 15;
+		int d = 20;
+		
+		System.out.println("a:" + b);
+		System.out.println(c + d);
+		System.out.println(a + b * c - d);
+	}
+
+}
--- src/com/mycompany/study/ch02/HelloWorld.java
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -4,13 +4,30 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		
+		/*
 		System.out.println("HelloWorld");
 		System.out.println("조대한");
 		System.out.println("25살");
 		System.out.println("운양동");
+		*/
 		
+		
+		// 변수 선언 : 데이터타입 변수명;
+		// 변수 초기화 : 데이터타입 변수명 = 리터럴;
+		
+		boolean a = true;
+		boolean b = false;
+		
+		int c = 1;
+		int d;
+		d = 1;
+		
+		int e,f,g;
+		e = 10;
+		System.out.println("e:" +e);
+		
+		System.out.println("c:"+c);
+
 		
 	}
 
