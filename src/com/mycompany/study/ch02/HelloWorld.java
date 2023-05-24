94c87f72ec6dc27546c7bc1284b78d2540fae4ab
panggin123 <panggin159@naver.com>
2023-05-24 15:35:32 +0900

    ㄴㅁㅇㄹ

---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("HelloWorld");
+		System.out.println("조대한");
+		System.out.println("25살");
+		System.out.println("운양동");
+		
+		
+	}
+
+}
