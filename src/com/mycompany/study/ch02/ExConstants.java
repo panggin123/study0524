314adf662eb5aa08bc776008c837808e1ae2cbe1
panggin123 <panggin159@naver.com>
2023-05-26 16:31:02 +0900

    dsaf

---
+++ src/com/mycompany/study/ch02/ExConstants.java
@@ -0,0 +1,18 @@
+package com.mycompany.study.ch02;
+
+public class ExConstants {
+
+	public static final String MY_NAME = "Tony";
+	public static final double PAI = 3.14;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	public static final String MY_NAME = "TONY";
+		
+		System.out.println("MY_NAME:" + MY_NAME);
+		
+		//MY_NAME = "Andrew";    //error
+	}
+
+}
