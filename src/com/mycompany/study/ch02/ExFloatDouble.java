af898722c3ad2aee7365c1c019ae60eed4a86cdd
panggin123 <panggin159@naver.com>
2023-05-26 17:32:28 +0900

    sdfafsdaf

--- src/com/mycompany/study/ch02/ExFloatDouble.java
+++ src/com/mycompany/study/ch02/ExFloatDouble.java
@@ -18,14 +18,28 @@ public class ExFloatDouble {
 		System.out.println("var1:" + var1);
 		System.out.println("var2:" + var2);
 		
+		System.out.println("--------------");
+		
 		System.out.println("var3:" + var3);
 		System.out.println("var4:" + var4);
 		System.out.println("var5:" + var5);
+		
+		System.out.println("--------------");
 		
 		double var6 = 1234567; 	// 7자리
 		double var7 = 12345678;	// 8자리 이상 
 		System.out.println("var6:" + var6);
 		System.out.println("var7:" + var7);
+		
+		float var8 = 3;
+		float var9 = 3f;
+		float var10 = 3F;
+		
+		System.out.println("--------------");
+		
+		System.out.println("var8:" + var8);
+		System.out.println("var9:" + var9);
+		System.out.println("var10:" + var10);
 	}
 
 }
