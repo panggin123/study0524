6f026392da0c0b933e2990172fe63ec1c273f750
panggin123 <panggin159@naver.com>
2023-05-31 17:38:18 +0900

    asdf

--- src/com/mycompany/study/ch04/ExIf_test2.java
+++ src/com/mycompany/study/ch04/ExIf_test2.java
@@ -41,6 +41,20 @@ public class ExIf_test2 {
 		if(grade==1) {
 			saleCost = cost*(1-0.05);
 				System.out.println("등급에 따라"+cost+"할인가 적용하여"+(int)saleCost);
+		}else if(grade==2) {
+			saleCost = cost*(1-0.1);
+			System.out.println("등급에 따라"+cost+"할인가 적용하여"+(int)saleCost);
+		}else if(grade==3) {
+			saleCost = cost*(1-0.15);
+			System.out.println("등급에 따라"+cost+"할인가 적용하여"+(int)saleCost);
+		}else if(grade==4) {
+			saleCost = cost*(1-0.2);
+			System.out.println("등급에 따라"+cost+"할인가 적용하여"+(int)saleCost);
+		}else if(grade==5) {
+			saleCost = cost*(1-0.027);
+			System.out.println("등급에 따라"+cost+"할인가 적용하여"+(int)saleCost);
+		}else {
+			
 		}
 		
 		
