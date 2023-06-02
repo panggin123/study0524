f5bf5b811093abd5f17053825c571c6ef293db51
panggin123 <panggin159@naver.com>
2023-06-02 17:38:38 +0900

    asdf

--- src/com/mycompany/study/ch05/ExArrayCrateByNew.java
+++ src/com/mycompany/study/ch05/ExArrayCrateByNew.java
@@ -18,6 +18,14 @@ public class ExArrayCrateByNew {
 		
 		int[]arr2;
 		arr2=new int[5];
+		
+		
+		
+		int[]arr3 = new int[3];
+		arr1[0] = 100;
+		arr1[1] = 99;
+		arr1[2] = 98;
+		System.out.println("arr3.length:" + arr3.length);
 	}
 
 }
