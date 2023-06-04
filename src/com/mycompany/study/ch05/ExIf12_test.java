505d097ada6e4dfb83f33b799379028e925fb5ae
user <user@DESKTOP-NPQ6R46>
2023-06-05 01:51:48 +0900

    asdf

---
+++ src/com/mycompany/study/ch05/ExIf12_test.java
@@ -0,0 +1,31 @@
+package com.mycompany.study.ch05;
+
+public class ExIf12_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=2; i<=9 ;i++) {
+			for(int j=1; j<=i; j++) {
+				if(i!=0 ) {
+					if(i % 2 == 0) {
+						System.out.println(i +"*"+j + "=" + i*j);
+					}
+						System.out.println("");
+					}else {
+						
+
+//					System.out.println("-------------------");
+				}
+
+			}
+
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/mycompany/study/ch05/Exfor123_test.java
@@ -0,0 +1,38 @@
+package com.mycompany.study.ch05;
+
+public class Exfor123_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		
+//		for(int i=0; i<5; i++) {
+//			for(int j=0; j<i; j++) {
+//			System.out.print("*");
+//		}
+//			System.out.println("*");
+//		}
+		
+		
+		
+//		for(int i=0; i<5; i++) {
+//			for(int j=i; j<5; j++) {
+//			System.out.print("*");
+//		}
+//			System.out.println("");
+//		}
+//		
+
+		System.out.println("1부터 10까지의 합");
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=i; j++) {
+					System.out.print(j+"+");
+				}
+				System.out.println();
+			}
+
+		}
+		
+		
+	}
+
