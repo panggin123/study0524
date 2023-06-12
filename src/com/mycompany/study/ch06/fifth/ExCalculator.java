8603a1ae7b3f074e744d96fd2ff11843d601f259
panggin123 <panggin159@naver.com>
2023-06-12 14:55:46 +0900

    asdf

---
+++ src/com/mycompany/study/ch06/fifth/ExCalculator.java
@@ -0,0 +1,32 @@
+package com.mycompany.study.ch06.fifth;
+
+public class ExCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//	Calculator calculator = new Calculator();
+	
+//		double result = calculator.areaSquare(5);
+//		double result2 = calculator.areaSquare(5,6);
+
+//		System.out.println("result:" + result);
+//		System.out.println("result2:" + result2);
+		
+//		Calculator calculator = new Calculator();
+		
+//		int result1 = calculator.plus(1, 2);
+//		int result2 = calculator.minus(1, 3);
+		
+		double result = 10 * Calculator.pi;
+		System.out.println("result:" + result);
+		
+		int result1 = Calculator.plus(1, 2);
+		int result2 = Calculator.minus(1, 3);
+		
+		System.out.println("result1:" + result1);
+		System.out.println("result2:" + result2);
+		
+	}
+
+}
