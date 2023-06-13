301713c55d26b515799a823aa83767dff29bdd82
panggin123 <panggin159@naver.com>
2023-06-13 17:30:32 +0900

    5123

--- src/com/mycompany/study/ch06/trgugu/TrExGugu.java
+++ src/com/mycompany/study/ch06/trgugu/TrExGugu.java
@@ -7,24 +7,38 @@ public class TrExGugu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+//		Scanner scanner = new Scanner(System.in);
+//		TrGugu gugu = new TrGugu();
+
+//		while(gugu.z) {
+//			System.out.println("정수를 입력해주세요");
+//			int scannerInt = scanner.nextInt(); // 입력받은 문자열을 str에 저장
+//			
+//			if(scannerInt !=0) {
+//				gugu.gugu(scannerInt);
+//				
+//			} else {
+//				System.out.println("프로그램 종료");
+//				gugu.z = false;
+//			}
+//		}
+
+		
 		Scanner scanner = new Scanner(System.in);
 		TrGugu gugu = new TrGugu();
-
+		
 		while(gugu.z) {
 			System.out.println("정수를 입력해주세요");
-			int scannerInt = scanner.nextInt(); // 입력받은 문자열을 str에 저장
-			
+			int scannerInt = scanner.nextInt();
+		
 			if(scannerInt !=0) {
 				gugu.gugu(scannerInt);
-				
+
 			} else {
 				System.out.println("프로그램 종료");
 				gugu.z = false;
 			}
 		}
-
-		
-
     }
 
 
--- src/com/mycompany/study/ch06/trgugu/TrGugu.java
+++ src/com/mycompany/study/ch06/trgugu/TrGugu.java
@@ -2,13 +2,26 @@ package com.mycompany.study.ch06.trgugu;
 
 public class TrGugu {
 
+//	boolean z = true;
+//	
+//	public void gugu(int num) {
+//		System.out.println("입력하시는 정수는:"+num);
+//		
+//		for(int i=1; i<=9; i++) {
+//		System.out.println(num+"*"+i+"="+num*i);
+//		}
+//		
+//		System.out.println("=============");
+//	}
+	
+	
 	boolean z = true;
 	
 	public void gugu(int num) {
 		System.out.println("입력하시는 정수는:"+num);
 		
 		for(int i=1; i<=9; i++) {
-		System.out.println(num+"*"+i+"="+num*i);
+			System.out.println(num+"*"+i+"="+num*i);
 		}
 		
 		System.out.println("=============");
