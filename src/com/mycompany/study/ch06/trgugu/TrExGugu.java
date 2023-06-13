822edfe3e021912fb8891d978281cce52127f9a3
panggin123 <panggin159@naver.com>
2023-06-13 17:17:22 +0900

    gugugugu

--- src/com/mycompany/study/ch06/trgugu/TrExGugu.java
+++ src/com/mycompany/study/ch06/trgugu/TrExGugu.java
@@ -6,19 +6,24 @@ public class TrExGugu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//
-//		Scanner scanner = new Scanner(System.in);
-//		
-//		int result = scanner.nextInt();
-//		System.out.println("result:" + result);
 
-		Gugu gugu = new Gugu();
-		for(int i=2; i<=9; i++) {
-			for(int j=1; j<=9; j++) {
-				System.out.println(i+"*"+j+"="+i*j);
+		Scanner scanner = new Scanner(System.in);
+		TrGugu gugu = new TrGugu();
+
+		while(gugu.z) {
+			System.out.println("정수를 입력해주세요");
+			int scannerInt = scanner.nextInt(); // 입력받은 문자열을 str에 저장
+			
+			if(scannerInt !=0) {
+				gugu.gugu(scannerInt);
+				
+			} else {
+				System.out.println("프로그램 종료");
+				gugu.z = false;
 			}
-			System.out.println("");
 		}
+
+		
 
     }
 
--- src/com/mycompany/study/ch07/third/ExSmartPhone.java
+++ src/com/mycompany/study/ch07/third/ExSmartPhone.java
@@ -5,7 +5,7 @@ public class ExSmartPhone {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//Phone phone = new Phone(); (X)
+		//Phone phone = new Phone(); (X)   abstract는 생성 하면 오류가 난다.
 		
 		SmartPhone smartPhone = new SmartPhone("smart");
 		
