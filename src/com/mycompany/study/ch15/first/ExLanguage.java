2e8ff11d39d35c3bdfad2009339d5421bfdc1e91
panggin123 <panggin159@naver.com>
2023-06-15 10:36:38 +0900

    asdfasdf

--- src/com/mycompany/study/ch15/first/ExLanguage.java
+++ src/com/mycompany/study/ch15/first/ExLanguage.java
@@ -9,34 +9,83 @@ public class ExLanguage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		List<String>list = new ArrayList<String>(); //generic
-//		String[]a;
-		List<String>list = new LinkedList<String>();       
 		
-		list.add("JAVA");
-		list.add("Python");
-		list.add("Node");
-		list.add(2,"C#");// 인덱스 위치를 지정해서 넣을때 
+		ArrayList<Language>arrayList = new ArrayList<Language>();
 		
-		
-		//size 데이터 총합 Array에 들어있는
-//		for(int i=0; i<list.size();i++) {
-//			System.out.println("list.get("+i+"):" +list.get(i));
-//		}
-//		System.out.println("-------------------");
-		
-		int i=0;
-		for(String a:list) {
-			// String a = list[i];
-			System.out.println(i+":"+a);
-			i++;
+		for(Language language:arrayList) {
+			System.out.println("language.name:"+language.name);
+			System.out.println("language.version:"+language.version);
 		}
 		
+//		arrayList.add("JAVA");  // Language 객체를 넣어 주어야 되어야 되서 오류
+//		arrayList.add(11);		// Language 객체를 넣어 주어야 되어야 되서 오류
 		
-		list.remove(2);
 		
+		Language language = new Language("JAVA", 11);
+		
+		arrayList.add(language);
 
+		for(Language a:arrayList) {
+			System.out.println("language.name:"+ a.name);
+			System.out.println("language.version:"+ a.version);
+			System.out.println("----------------------");
+		}
 		
+//		
+//		ArrayList<Language>arrayList = new ArrayList<Language>();
+//		
+//		for(Language language :arrayList) {
+//			System.out.println(language.name);
+//			System.out.println(language.version);
+//		}
+//		
+//		Language language = new Language("JAVA", 11);
+//		
+//		arrayList.add(language);
+//		
+//		for(Language a: arrayList) {
+//			System.out.println(language.name);
+//			System.out.println(language.version);
+//			
+//		}
+		
+		
+//		List<String>list = new ArrayList<String>(); //generic
+//		String[]a;
+//		List<String>list = new LinkedList<String>();       
+//		
+//		list.add("JAVA");
+//		list.add("Python");
+//		list.add("Node");
+//		list.add(2,"C#");// 인덱스 위치를 지정해서 넣을때 
+//		
+//		
+//		//size 데이터 총합 Array에 들어있는
+////		for(int i=0; i<list.size();i++) {
+////			System.out.println("list.get("+i+"):" +list.get(i));
+////		}
+////		System.out.println("-------------------");
+//		
+//		int i=0;
+//		for(String a:list) {
+//			// String a = list[i];
+//			System.out.println(i+":"+a);
+//			i++;
+//		}
+//		System.out.println("-------------------");
+//		
+//		list.remove(2); //삭제 
+//		list.remove("Python");
+//		
+//		
+//		int j=0;
+//		for(String a:list) {
+//			// String a = list[i];
+//			System.out.println(j+":"+a);
+//			j++;
+//		}
+		
+		// 오브젝트 시조새 제일 높은 클래스?
 		
 		
 //		ArrayList<String> arrayList = new ArrayList<String>();
