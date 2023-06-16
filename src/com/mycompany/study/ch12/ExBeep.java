d0b42043e07f17e7f85c38609a3d711a29461aa3
panggin123 <panggin159@naver.com>
2023-06-16 09:36:58 +0900

    a1a

--- src/com/mycompany/study/ch12/ExBeep.java
+++ src/com/mycompany/study/ch12/ExBeep.java
@@ -1,17 +1,17 @@
 package com.mycompany.study.ch12;
 
-import java.awt.Toolkit;
+import java.awt.Toolkit;  // 순수한 자바프로그램을 만드는 것 awt
 
 public class ExBeep {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		
+		Toolkit toolkit = Toolkit.getDefaultToolkit();      // get은 받아들인다는 의미
+															// set은 세팅을 한다는 의미
 		for(int i=0; i<5; i++) {		
-			toolkit.beep();
-//			Thread.sleep(500);
+			toolkit.beep();   //소리내는 함수
+			Thread.sleep(500);  
 			
 		}
 	}
