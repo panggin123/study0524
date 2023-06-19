0d35f5f2c559820adf4c40c6debbe7eeb1753f6d
panggin123 <panggin159@naver.com>
2023-06-19 15:02:09 +0900

    asdfasdf

--- src/com/mycompany/study/test123/test1/Computer.java
+++ src/com/mycompany/study/test123/test1/Computer.java
@@ -1,5 +1,10 @@
 package com.mycompany.study.test123.test1;
 
-public class Computer {
+public class Computer extends Calculator{
 
+	@Override
+	public double ci(double r) {
+		
+		return r*r*Constants.PA_2;
+	}
 }
--- src/com/mycompany/study/test123/test1/ComputerExample.java
+++ src/com/mycompany/study/test123/test1/ComputerExample.java
@@ -5,6 +5,16 @@ public class ComputerExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int r = 7;
+		
+		Calculator calculator = new Calculator();
+		Computer computer = new Computer();
+		
+		System.out.println(r);
+		System.out.println("Calculator:"+calculator.ci(r));
+		System.out.println(r);
+		System.out.println(computer.ci(r));
+		System.out.println(r);
 	}
 
 }
